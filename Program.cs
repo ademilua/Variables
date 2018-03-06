79b52fd313290e7f2737cbc0cc347c8a22cddf4c
Tolulope <tolesy2003@outlook.com>
2018-03-06 15:08:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /*  int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+           
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+            Console.Write("What is your name?");
+            Console.Write("Type your name:");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            // string myLastName;
+            // Console.Write("Type your last name: ");
+            // myLastName = Console.ReadLine();
+            Console.Write("What is your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, "+ myFirstName +" "+ myLastName);
+            Console.ReadLine();
+        }
+    }
+}
